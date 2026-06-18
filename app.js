712940495745f88b55d1a067e2cd4c45c6f4cf8d
Nijin <nijuvenghat550649@gmail.com>
2026-06-18 16:20:46 +0530

    show sign in button text on mobile nav, remove sign-out icon

--- app.js
+++ app.js
@@ -85,16 +85,15 @@ function updateNavAuthButton() {
       <span>Dashboard</span>
     `;
     btnDashboardNav.title = "View Dashboard";
+    btnDashboardNav.classList.remove('auth-signed-out');
+    btnDashboardNav.classList.add('auth-signed-in');
   } else {
     btnDashboardNav.innerHTML = `
-      <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
-        <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"/>
-        <polyline points="10 17 15 12 10 7"/>
-        <line x1="15" y1="12" x2="3" y2="12"/>
-      </svg>
       <span>Sign In</span>
     `;
     btnDashboardNav.title = "Sign In with Google";
+    btnDashboardNav.classList.remove('auth-signed-in');
+    btnDashboardNav.classList.add('auth-signed-out');
   }
 }
 
